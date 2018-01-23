7de81ec4d346314ce311d099d49faef6e99acebe
Harold <pxduharold@126.com>
2018-01-23 15:22:25 +0800

    backend realization of events duplication check is not so good next try frontend check

--- test_appointment/test_fullcalendar/main.py
+++ test_appointment/test_fullcalendar/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request, render_template, jsonify
 import json
 from flask_bootstrap import Bootstrap
+import time
 
 
 app = Flask(__name__)
@@ -23,11 +24,11 @@ def return_data():
     # but since no db or any real storage is implemented I'm just
     # returning data from a text file that contains json elements
 
-    with open("events.json", "r") as input_data:
+    with open("test.json", "r") as input_data:
         # you should use something else here than just plaintext
         # check out jsonfiy method or the built in json module
         # http://flask.pocoo.org/docs/0.10/api/#module-flask.json
-        return input_data.read()
+        return "[" + input_data.read() + "]"
 
 
 @app.route('/add', methods=['POST'])
@@ -35,8 +36,19 @@ def add_data():
     r = request.get_json(force=True)
     start_date = r['start']
     end_date = r['end']
-    print(start_date, end_date)
-    return jsonify({'test': 1})
+    title = r['title']
+    with open('test.json', 'r') as f:
+        events_dict = json.load(f)
+    # print(events_dict['title'])
+    for event in events_dict:
+        if title == event['title'] and start_date == event['start']:
+            print("Invalid")
+            return jsonify({'blocked': 1})
+    print(title, start_date, end_date)
+    with open('events.json', 'a') as f:
+        f.write(',')
+        json.dump(r, f)
+    return jsonify({'blocked': 0})
 
 
 if __name__ == '__main__':
