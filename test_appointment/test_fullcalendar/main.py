05b68b37e7b2b75113ed95d7c638b1fbab074657
Harold <pxduharold@126.com>
2018-01-23 17:50:04 +0800

    datetime.strptime(momentjs_Date, '%Y-%m-%dT%H:%M:%S.%fZ') to convert js datetime to python datetime

--- test_appointment/test_fullcalendar/main.py
+++ test_appointment/test_fullcalendar/main.py
@@ -3,11 +3,42 @@ from flask import request, render_template, jsonify
 import json
 from flask_bootstrap import Bootstrap
 import time
+from flask_sqlalchemy import SQLAlchemy
+import os
+from datetime import datetime, date
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              ('mysql://flask_test:123456@localhost/flask_test')
 
 
 app = Flask(__name__)
+# app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask_test:123456@localhost/flask_test'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bootstrap = Bootstrap()
 bootstrap.init_app(app)
+db = SQLAlchemy(app)
+# db.init_app(app)  # to create table first, cannot use db.init_app(app), why???
+
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    start = db.Column(db.DateTime(), default=datetime.utcnow)
+    end = db.Column(db.DateTime())
+
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __repr__(self):
+        return '<Event %r>' % self.id
 
 
 @app.route('/')
@@ -31,26 +62,33 @@ def return_data():
         return "[" + input_data.read() + "]"
 
 
+'''date_handler = lambda obj: (
+    obj.isoformat()
+    if isinstance(obj, (datetime, date))
+    else None
+)
+json.dumps(datetime.now(), default=date_handler)'''
+
+
 @app.route('/add', methods=['POST'])
 def add_data():
     r = request.get_json(force=True)
-    start_date = r['start']
-    end_date = r['end']
+    start_date = datetime.strptime(r['start'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    end_date = datetime.strptime(r['end'], '%Y-%m-%dT%H:%M:%S.%fZ')
     title = r['title']
-    with open('test.json', 'r') as f:
-        events_dict = json.load(f)
-    # print(events_dict['title'])
-    for event in events_dict:
-        if title == event['title'] and start_date == event['start']:
+    events = Event.query.all()
+    for event in events:
+        if start_date <= event.start <= event.end:  # TypeError: unorderable types: str() <= datetime.datetime()
             print("Invalid")
             return jsonify({'blocked': 1})
     print(title, start_date, end_date)
-    with open('events.json', 'a') as f:
-        f.write(',')
-        json.dump(r, f)
+    event_new = Event(start=start_date, end=end_date)
+    db.session.add(event_new)
+    db.session.commit()
     return jsonify({'blocked': 0})
 
 
 if __name__ == '__main__':
+    # db.create_all()  # execute from python console but still failed...
     app.debug = True
     app.run()
