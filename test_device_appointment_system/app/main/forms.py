e4c0c77c23189458ef07bae5f6e57bf0ba64723c
Harold <pxduharold@126.com>
2018-01-26 17:32:54 +0800

    include flask-table to admin edit page

--- test_device_appointment_system/app/main/forms.py
+++ test_device_appointment_system/app/main/forms.py
@@ -4,13 +4,29 @@ from wtforms import StringField, TextAreaField, BooleanField, SelectField, \
 from wtforms.validators import Required, Length, Email, Regexp
 from wtforms import ValidationError
 from ..models import Role, User
+from flask_table import Table, Col
 
 
 class EditUserForm(FlaskForm):
-    name = StringField('Real name', validators=[Required(), Length(1, 64),
+    name = StringField('Real name', validators=[Length(1, 64),
                                                 Regexp('^[A-Za-z][A-Za-z\s]*$', 0,
                                                        'Names must have only letters, '
                                                        'or space')])
     email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
     role = SelectField('Role', coerce=int)
+    device = TextAreaField('Device')
     submit = SubmitField('Submit')
+
+
+class ItemTable(Table):
+    name = Col('Name')
+    id = Col('id')
+    classes = ['table', 'table-bordered']
+    devices = Col('devices')
+
+
+class Item(object):
+    def __init__(self, name, id, devices):
+        self.name = name
+        self.id = id
+        self.devices = devices
--- test_device_appointment_system/app/main/views.py
+++ test_device_appointment_system/app/main/views.py
@@ -4,16 +4,14 @@ from .. import db
 from ..models import User, Device, Permission
 from flask_login import login_required, current_user
 from ..decorators import admin_required, permission_required
-from .forms import EditUserForm
+from .forms import EditUserForm, Item, ItemTable
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     show_edit = False
-    if current_user.is_authenticated:
-        show_edit = bool(request.cookies.get('show_edit', ''))
-    if show_edit:
-        pass
+    if current_user.is_administrator:
+        show_edit = True
     return render_template("index.html", show_edit=show_edit)
 
 
@@ -27,5 +25,12 @@ def calendar():
 @admin_required
 def edit():
     form = EditUserForm()
+    user = None
     if form.validate_on_submit():
-        pass
+        email = form.email.data
+        device = form.device.data
+        print(device)
+        name = form.email.name
+        role = form.email.role
+        user = User.query.get_or_404(email=email)
+    return render_template('edit.html', form=form, user=user)
