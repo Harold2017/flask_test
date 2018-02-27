c171da4ae454d4694602fa78e2a5c519174c7d4a
Harold <pxduharold@126.com>
2018-02-27 17:09:03 +0800

    generate a QR code when add new device

--- test_device_appointment_system/app/QRcode/QRcode.py
+++ test_device_appointment_system/app/QRcode/QRcode.py
@@ -11,7 +11,7 @@ qr = qrcode.QRCode(version=1,
 
 
 def qr_generator(device_id):
-    data = baseUrl + '/form' + str(device_id)
+    data = baseUrl + '/form/' + str(device_id)
     qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image()
