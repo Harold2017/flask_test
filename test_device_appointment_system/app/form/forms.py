49946166434145e6f8e5f6297931877ce403a5cb
Harold <pxduharold@126.com>
2018-03-02 10:35:58 +0800

    generate a QR code when add new device fill in a form to add log to corresponding device when scan the QR code

--- test_device_appointment_system/app/form/forms.py
+++ test_device_appointment_system/app/form/forms.py
@@ -2,6 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SelectField, \
     SubmitField
 from wtforms.validators import Required, Length
+from flask_table import Table, Col
+'''from pytz import timezone
+
+
+tzchina = timezone('Asia/Shanghai')
+utc = timezone('UTC')'''
 
 
 class LogForm(FlaskForm):
@@ -11,3 +17,28 @@ class LogForm(FlaskForm):
                          validators=[Required()])
     details = TextAreaField('Details')
     submit = SubmitField('Submit')
+
+
+class ItemTable(Table):
+    user_name = Col('Username')
+    device_id = Col('Device_id')
+    device_name = Col('Device_name')
+    classes = ['table', 'table-bordered']
+    device_status = Col('Device_status')
+    log_time = Col('Log_time')
+    details = Col('Details')
+
+
+class Item(object):
+    def __init__(self, user_name, device_id, device_name, device_status, log_time, details):
+        self.user_name = user_name
+        self.device_id = device_id
+        # self.device_status = {0: None, 1: 'Normal', 2: 'Broken'}.get(device_status)
+        # self.log_time = log_time.replace(tzinfo=utc).astimezone(tzchina).strftime('%Y/%m/%d-%H:%M:%S')
+        self.details = details
+        # Username	Device_id	Device_status	Log_time	Details
+        # Ivan Lau	15	        True	        2018-03-01 07:08:26
+        self.device_status = device_status
+        self.log_time = log_time
+        self.device_name = device_name
+
--- test_device_appointment_system/app/form/views.py
+++ test_device_appointment_system/app/form/views.py
@@ -1,12 +1,16 @@
 from . import form
-from .forms import LogForm
+from .forms import LogForm, ItemTable
 from flask import render_template
 from datetime import datetime
 import os
 import json
-from ..models import UserLog
+from ..models import UserLog, Device
 from .. import db
+from pytz import timezone
 
+
+tzchina = timezone('Asia/Shanghai')
+utc = timezone('UTC')
 
 log_folder = os.path.abspath('app') + '\\form\\log\\'
 
@@ -22,7 +26,8 @@ def log(device_id):
         status = s.get(status)
         details = form.details.data
         log_time = datetime.utcnow()
-        user_log = UserLog(user_name=user, device_id=device_id, device_status=status, log_time=log_time, details=details)
+        user_log = UserLog(user_name=user, device_id=device_id, device_status=status, log_time=log_time,
+                           details=details)
         db.session.add(user_log)
         db.session.commit()
         return render_template('log/success.html')
@@ -48,3 +53,26 @@ def log(device_id):
             f.write('\n')
         return render_template('log/success.html')
     return render_template('log/log.html', form=form)'''
+
+
+@form.route('/device_log/<device_id>')
+def device_log(device_id):
+    device_id = int(device_id)
+    if UserLog.query.filter_by(device_id=device_id).first():
+        d_logs = UserLog.query.filter_by(device_id=device_id).all()
+        device_name = Device.query.filter_by(id=device_id).first().name
+        ls = []
+        for d_log in d_logs:
+            d = {'user_name': d_log.user_name,
+                 'device_id': d_log.device_id,
+                 'device_name': device_name,
+                 'device_status': {0: None, 1: 'Normal', 2: 'Broken'}.get(d_log.device_status),
+                 'log_time': d_log.log_time.replace(tzinfo=utc).astimezone(tzchina).strftime('%Y/%m/%d-%H:%M:%S'),
+                 'details': d_log.details}
+            ls.append(d)
+        table = ItemTable(ls)
+        warn = 0
+    else:
+        warn = 'No logs for this device!'
+        table = 0
+    return render_template('log/device_log.html', warn=warn, table=table)
