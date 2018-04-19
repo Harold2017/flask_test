b822c23932c6053ab090ca66a5b2c7350905c11d
Harold <pxduharold@126.com>
2018-04-19 14:34:13 +0800

    limit displaying logs to 5 or within 2 days

--- test_device_appointment_system/app/form/views.py
+++ test_device_appointment_system/app/form/views.py
@@ -2,7 +2,7 @@ from . import form
 from .forms import StartForm, EndForm, ItemTable, GloveBoxStartForm, \
         GloveBoxEndForm, GloveItemTable, StateTransferForm
 from flask import render_template
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 import json
 from ..models import Device, DeviceUsageLog, GloveBoxLog
@@ -118,7 +118,11 @@ def log(device_id):
 def device_log(device_id):
     device_id = int(device_id)
     if DeviceUsageLog.query.filter_by(device_id=device_id).first():
-        d_logs = DeviceUsageLog.query.filter_by(device_id=device_id).all()
+        d_logs = DeviceUsageLog.query.filter_by(device_id=device_id).filter(DeviceUsageLog.start_time <= datetime.utcnow().date()).filter(DeviceUsageLog.start_time >= (datetime.utcnow().date() - timedelta(days=2))).all()
+        if len(d_logs) >= 5:
+            d_logs = d_logs[-5:]
+        else:
+            d_logs = DeviceUsageLog.query.filter_by(device_id=device_id).limit(5).all()
         device_name = Device.query.filter_by(id=device_id).first().name
         ls = []
         for d_log in d_logs:
@@ -235,7 +239,11 @@ def glovebox(device_id):
 def glovebox_log(device_id):
     device_id = int(device_id)
     if GloveBoxLog.query.filter_by(device_id=device_id).first():
-        g_logs = GloveBoxLog.query.filter_by(device_id=device_id).all()
+        g_logs = GloveBoxLog.query.filter_by(device_id=device_id).filter(GloveBoxLog.start_time <= datetime.utcnow().date()).filter(GloveBoxLog.start_time >= (datetime.utcnow().date() - timedelta(days=2))).all()
+        if len(g_logs) >= 5:
+            g_logs = g_logs[-5:]
+        else:
+            g_logs = GloveBoxLog.query.filter_by(device_id=device_id).limit(5).all()
         device_name = Device.query.filter_by(id=device_id).first().name
         ls = []
         for g_log in g_logs:
