f617ff67430c3b0e5b87bf343e7bad21a58eb94a
Harold <pxduharold@gmail.com>
2019-01-18 13:56:45 +0800

    test with celery

--- test_celery/app/main/views.py
+++ test_celery/app/main/views.py
@@ -5,8 +5,3 @@ from . import main
 @main.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
-
-
-@main.route('/user', methods=['GET', 'POST'])
-def user():
-    pass
